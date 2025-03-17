605827ac1b3ae799683593c80d6a6103420cd4b2
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-03-17 18:50:10 +0100

    Initial Commit

---
+++ Snapshot/SnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotApp.swift
+//  Snapshot
+//
+//  Created by Javier Calatrava on 17/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapshotTests/SnapshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotTests.swift
+//  SnapshotTests
+//
+//  Created by Javier Calatrava on 17/3/25.
+//
+
+import Testing
+@testable import Snapshot
+
+struct SnapshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
