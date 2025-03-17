73ca03f58c897a746da44978511500c248270d14
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-03-17 20:10:02 +0100

    Base project

--- Snapshot/ContentView.swift
+++ Snapshot/ContentView.swift
@@ -7,18 +7,97 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
+    @State var navigationPath = NavigationPath()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $navigationPath) {
+            ContentView(navigationPath: $navigationPath)
+                .navigationDestination(for: String.self) { destination in
+                    switch destination {
+                    case "SecondView":
+                        SecondView(navigationPath: $navigationPath)
+                    case "ThirdView":
+                        ThirdView(navigationPath: $navigationPath)
+                    default:
+                        EmptyView()
+                    }
+                }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    @Binding var navigationPath: NavigationPath
+
+    var body: some View {
+        VStack {
+            Text("First View")
+                .font(.largeTitle)
+                .padding()
+            
+            Button(action: {
+                navigationPath.append("SecondView")
+            }) {
+                Text("Go to second viee")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .accessibilityIdentifier("incrementButton")
+        }
+        .navigationTitle("First View")
+    }
+    
+    func testButtonPress() {
+        navigationPath.append("SecondView")
+    }
+}
+
+struct SecondView: View {
+    @Binding var navigationPath: NavigationPath
+
+    var body: some View {
+        VStack {
+            Text("Second View")
+                .font(.largeTitle)
+                .padding()
+            
+            Button(action: {
+                navigationPath.append("ThirdView")
+            }) {
+                Text("Go to third view")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .navigationTitle("Second View")
+    }
+}
+
+struct ThirdView: View {
+    @Binding var navigationPath: NavigationPath
+
+    var body: some View {
+        VStack {
+            Text("Third View")
+                .font(.largeTitle)
+                .padding()
+            
+            Button(action: {
+                // Pop to root
+                navigationPath.removeLast(navigationPath.count) // Empty stack
+            }) {
+                Text("Get back to first view")
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .navigationTitle("Third view")
+    }
 }
--- SnapshotTests/SnapshotTests.swift
+++ SnapshotTests/SnapshotTests.swift
@@ -5,13 +5,72 @@
 //  Created by Javier Calatrava on 17/3/25.
 //
 
+import SwiftUI
+import SnapshotTesting
 import Testing
 @testable import Snapshot
 
-struct SnapshotTests {
+protocol TestDevice {
+    func viewImageConfig() -> ViewImageConfig
+}
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct iPhoneSe: TestDevice  {
+     func viewImageConfig() -> ViewImageConfig {
+        ViewImageConfig.iPhoneSe
     }
+}
 
+struct iPhone13ProMax: TestDevice  {
+     func viewImageConfig() -> ViewImageConfig {
+        ViewImageConfig.iPhone13ProMax(.portrait)
+    }
+}
+
+struct iPhone12Landscape: TestDevice  {
+     func viewImageConfig() -> ViewImageConfig {
+         ViewImageConfig.iPhone12(.landscape)
+    }
+}
+
+
+@MainActor
+@Suite("Snapshot tests")
+struct SnapshotTests {
+    
+    var record = false // SNAPSHOT TESTING MODE!
+    static let devices: [TestDevice] = [iPhoneSe(), iPhone13ProMax(), iPhone12Landscape()]
+    
+    @Test(arguments: devices) func testFirstView(device: TestDevice) {
+        let rootView = RootView()
+        let hostingController = UIHostingController(rootView: rootView)
+        var named = String(describing: type(of: device))
+        assertSnapshot(of: hostingController,
+                       as: .image(on: device.viewImageConfig()),
+                       named: named,
+                       record: record)
+    }
+    
+    @Test(arguments: devices) func testSecondView(device: TestDevice) {
+        let secondView = SecondView(navigationPath: .constant(NavigationPath()))
+        let hostingController = UIHostingController(rootView: secondView)
+
+        var named = String(describing: type(of: device))
+        
+        assertSnapshot(of: hostingController,
+                       as: .image(on: device.viewImageConfig()),
+                       named: named,
+                       record: record)
+    }
+    
+    @Test(arguments: devices) func testThirdView(device: TestDevice) {
+        let thirdView = ThirdView(navigationPath: .constant(NavigationPath()))
+        let hostingController = UIHostingController(rootView: thirdView)
+
+        var named = String(describing: type(of: device))
+        
+        assertSnapshot(of: hostingController,
+                       as: .image(on: device.viewImageConfig()),
+                       named: named,
+                       record: record)
+    }
 }
